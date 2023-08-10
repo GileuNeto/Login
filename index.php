9fed26198ce3589bdcc7519e0d38d6b91941fe74
Gileu Neto <gileuneto@gmail.com>
2023-08-10 09:08:32 -0300

    login

--- index.php
+++ index.php
@@ -6,6 +6,34 @@
     <title>Login</title>
 </head>
 <body>
-    
+    <div class="container-image">
+        <img src="_cdn/images/image.svg" alt="">
+    </div>
+    <div class="container-login">
+        <form class="form">
+            <div class="form-title">
+                <p>Bem-Vindo de Volta</p>
+                <h1>Faça login na sua conta</h1>
+            </div>
+            <div class="form-login">
+                <label for="email">E-mail</label>
+                <input type="email" id="email" placeholder="exemple@gmail.com">
+                <label for="senha">Senha</label>
+                <input type="passward " id="senha" placeholder="********">
+            </div>
+            <div class="form-radio">
+                <input type="radio" id="lembrar-login">
+                <label for="lembrar-login">Lembrar de mim</label>
+                <a href="#">Esqueceu sua senha?</a>
+            </div>
+            <div class="form-button">
+                <button class="btn btn-login">Entrar</button>
+                <button class="btn btn-login-google"><img src="_cdn/images/google.svg" alt="image-google">ou faça login com o Google</button>
+            </div>
+            <div class="cadastro">
+                <p>Não tem uma conta?<a href="#">Cadastre-se</a></p>
+            </div>
+        </form>
+    </div>    
 </body>
 </html>
